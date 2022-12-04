76ada2c7ce876d2d7c17dd97c475ebe08208ef31
sdf <saidovabbos1995@gmail.com>
2022-12-04 21:22:28 +0500

    everything is awesome

--- lib/animate.dart
+++ lib/animate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class AnimatedLogo extends AnimatedWidget {
   const AnimatedLogo({super.key, required Animation<double> animation})
@@ -6,26 +7,19 @@ class AnimatedLogo extends AnimatedWidget {
   static int t=0;
 
   // Make the Tweens static because they don't change.
-  static final _opacityTween = Tween<double>(begin: 0.5, end: 1);
+  static final _opacityTween = Tween<double>(begin: 0, end: 2);
   //static final _sizeTween = Tween<double>(begin: 0, end: 300);
 
   @override
   Widget build(BuildContext context){
     t=t+1;
-    print("t=$t");
     final animation = listenable as Animation<double>;
-    return Center(
-      child:Transform.scale(
-        scale: _opacityTween.evaluate(animation),
-        child:  Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              height:200, //_sizeTween.evaluate(animation),
-              width:200, //_sizeTween.evaluate(animation),
-              child: const FlutterLogo(),
-            ),
-      )
-
-
+    print(_opacityTween.evaluate(animation).toString()+" angle="+math.pi.toString());
+    return Stack(
+      alignment: Alignment.center,
+      children:[
+        SizedBox(height: 400,width: 400,child: Image.asset("assets/icosn/testGif3.gif"),),
+      ]
       ///opacity
     //  child: Opacity(
     //    opacity: _opacityTween.evaluate(animation),
@@ -55,11 +49,11 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+    animation = CurvedAnimation(parent: controller, curve: Curves.linear)
       ..addStatusListener((status) {
 
         if (status == AnimationStatus.completed) {
-          controller.reverse();
+          controller.repeat(period:const Duration(seconds: 2));
         } else if (status == AnimationStatus.dismissed) {
           controller.forward();
         }
